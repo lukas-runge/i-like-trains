f71b55f80c1a47b81421cde43b765d76cf35b05c
Lukas Runge <login@lukasrunge.de>
2024-09-19 18:11:00 +0200

    ✨ add multithreading

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -10,6 +10,8 @@
 #include <string>
 
 #include "message.pb.h"
+#include "pico/multicore.h"
+#include "pico/util/queue.h"
 #include "pico/stdlib.h"
 
 // #define WAIT_TIME_1 58
@@ -95,15 +97,84 @@ void sendCommand(uint8_t address, uint8_t command) {
     output_bit(1);
 }
 
-uint32_t get32BitUIntFromStdin() {
-    uint32_t unsignedInt;
+uint32_t readMessageLength() {
+    uint32_t messageLength;
 
-    unsignedInt = getchar();
-    unsignedInt |= getchar() << 8;
-    unsignedInt |= getchar() << 16;
-    unsignedInt |= getchar() << 24;
+    messageLength = getchar();
+    messageLength |= getchar() << 8;
+    messageLength |= getchar() << 16;
+    messageLength |= getchar() << 24;
 
-    return unsignedInt;
+    return messageLength;
+}
+
+void sendMessageLength(uint32_t messageLength) {
+    putchar(messageLength & 0xFF);
+    putchar((messageLength >> 8) & 0xFF);
+    putchar((messageLength >> 16) & 0xFF);
+    putchar((messageLength >> 24) & 0xFF);
+}
+
+void sendMessage(uint8_t *buffer, uint32_t messageLength) {
+    for (uint32_t i = 0; i < messageLength; i++) {
+        putchar(buffer[i]);
+    }
+}
+
+struct {
+    uint32_t messageLength;
+    uint8_t buffer[SERIAL_MESSAGE_PB_H_MAX_SIZE];
+} Message;
+
+queue_t messageQueue;
+
+void InputHandler() {
+    while (true) {
+        uint32_t messageLength = readMessageLength();
+
+        uint8_t buffer[messageLength];
+        for (uint32_t i = 0; i < messageLength; i++) {
+            buffer[i] = getchar();
+        }
+
+        pb_istream_t stream = pb_istream_from_buffer(buffer, messageLength);
+
+        serial_Message message = serial_Message_init_zero;
+
+        if (!pb_decode(&stream, serial_Message_fields, &message)) {
+            // printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
+            continue;
+        }
+        
+        queue_add_blocking(&messageQueue, &message);
+
+        gpio_put(LED_PIN, gpio_get(LED_PIN) ^ 1);
+    }
+}
+
+void handleQueue() {
+    serial_Message message;
+
+    if (queue_try_remove(&messageQueue, &message)) {
+        if (message.which_content == serial_Message_handshake_tag) {
+            serial_Message response = serial_Message_init_zero;
+
+            uint8_t buffer[SERIAL_MESSAGE_PB_H_MAX_SIZE];
+
+            pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+
+            response.which_content = serial_Message_handshake_tag;
+            response.content.handshake = serial_Handshake_init_default;
+            response.content.handshake.type = serial_HandshakeType_RESPONSE;
+
+            if (!pb_encode(&stream, serial_Message_fields, &response)) {
+                return;
+            }
+
+            sendMessageLength(stream.bytes_written);
+            sendMessage(buffer, stream.bytes_written);
+        }
+    }
 }
 
 int main() {
@@ -121,97 +192,52 @@ int main() {
     gpio_set_dir(PLUS, GPIO_OUT);
     gpio_put(PLUS, 1);
 
+    queue_init(&messageQueue, sizeof(Message), 10);
+
+    multicore_launch_core1(InputHandler);
+
     auto time = to_ms_since_boot(get_absolute_time());
 
-    // while (true)
-    // {
-    //     printf("Hello, world!\n");
-    //     auto newTime = to_ms_since_boot(get_absolute_time());
-    //     if (newTime - time > 10000)
-    //     {
-    //         break;
-    //     }
-    // }
-
     while (true) {
-        uint32_t messageLength = get32BitUIntFromStdin();
+        while (true) {
+            sendCommand(IDLE_ADDRESS, IDLE_COMMAND);
+            handleQueue();
 
-        uint8_t buffer[messageLength];
-        for (uint32_t i = 0; i < messageLength; i++) {
-            buffer[i] = getchar();
-        }
+            auto newTime = to_ms_since_boot(get_absolute_time());
 
-        pb_istream_t stream = pb_istream_from_buffer(buffer, messageLength);
-
-        serial_Message message = serial_Message_init_zero;
-
-        if (!pb_decode(&stream, serial_Message_fields, &message)) {
-            printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
-            continue;
-        }
-
-        if (message.which_content == serial_Message_handshake_tag) {
-            serial_Message response = serial_Message_init_zero;
-
-            uint8_t buffer[SERIAL_MESSAGE_PB_H_MAX_SIZE];
-
-            pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-
-            response.which_content = serial_Message_handshake_tag;
-            response.content.handshake = serial_Handshake_init_default;
-            response.content.handshake.type = serial_HandshakeType_RESPONSE;
-
-            if (!pb_encode(&stream, serial_Message_fields, &response)) {
-                printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
-                continue;
-            }
-
-            printf("%c%c%c%c", (stream.bytes_written >> 0) & 0xFF, (stream.bytes_written >> 8) & 0xFF, (stream.bytes_written >> 16) & 0xFF, (stream.bytes_written >> 24) & 0xFF);
-            for (uint32_t i = 0; i < stream.bytes_written; i++) {
-                putchar(buffer[i]);
+            if (newTime - time > 1000) {
+                break;
             }
         }
-    }
 
-    while (true) {
-        sendCommand(IDLE_ADDRESS, IDLE_COMMAND);
+        time = to_ms_since_boot(get_absolute_time());
 
-        auto newTime = to_ms_since_boot(get_absolute_time());
+        sendCommand(ADDRESS, FORWARD);
 
-        if (newTime - time > 1000) {
-            break;
+        while (true) {
+            sendCommand(IDLE_ADDRESS, IDLE_COMMAND);
+            handleQueue();
+
+            auto newTime = to_ms_since_boot(get_absolute_time());
+
+            if (newTime - time > 5000) {
+                break;
+            }
         }
-    }
 
-    time = to_ms_since_boot(get_absolute_time());
+        time = to_ms_since_boot(get_absolute_time());
 
-    sendCommand(ADDRESS, FORWARD);
+        sendCommand(ADDRESS, HALT);
 
-    while (true) {
-        sendCommand(IDLE_ADDRESS, IDLE_COMMAND);
+        while (true) {
+            sendCommand(IDLE_ADDRESS, IDLE_COMMAND);
+            handleQueue();
 
-        auto newTime = to_ms_since_boot(get_absolute_time());
+            auto newTime = to_ms_since_boot(get_absolute_time());
 
-        if (newTime - time > 5000) {
-            break;
+            if (newTime - time > 5000) {
+                break;
+            }
         }
-    }
-
-    time = to_ms_since_boot(get_absolute_time());
-
-    sendCommand(ADDRESS, HALT);
-
-    while (true) {
-        sendCommand(IDLE_ADDRESS, IDLE_COMMAND);
-
-        auto newTime = to_ms_since_boot(get_absolute_time());
-
-        if (newTime - time > 5000) {
-            break;
-        }
-    }
-
-    while (true) {
-        sendCommand(IDLE_ADDRESS, IDLE_COMMAND);
     }
 }
